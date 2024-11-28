0558e4220fc8cf788925d542a4d9dde8558ccf2f
Tauhid hossen <tauhidhossen04@gmail.com>
2024-11-28 13:04:58 +0600

    Banner section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/components/Banner";
 
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-2xl font-bold text-yellow-600">Hellow</h1>
-   </div>
+    <div>
+      <Banner />
+    </div>
   );
 }
